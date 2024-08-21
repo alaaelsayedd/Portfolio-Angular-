3ffa5abf6fef40315727e51b1fc715109868d676
AlaaElsayed <salaaelsayed@gmail.com>
2024-08-21 15:39:54 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./Components/header/header.component";
+import { NavbarComponent } from "./Components/navbar/navbar.component";
+import { AboutComponent } from "./Components/about/about.component";
+import { SkillsComponent } from "./Components/skills/skills.component";
+import { PortfolioComponent } from "./Components/portfolio/portfolio.component";
+import { FooterComponent } from "./Components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, AboutComponent, SkillsComponent, PortfolioComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
